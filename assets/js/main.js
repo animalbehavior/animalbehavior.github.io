d13ecb994c225b6946301ff2b5ddb9b0ded95844
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-06-16 18:36:06 -0400

    Bind to resize event

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,10 @@
     'use strict';
 
     // Make body flush with top of viewport
-    document.body.style.marginTop = document.body.scrollHeight - document.documentElement.scrollHeight + 'px';
+    window.onresize = () => {
+        document.body.style.marginTop = 0;
+        document.body.style.marginTop = document.body.scrollHeight - document.documentElement.scrollHeight + 'px';
+    };
+
+    window.onresize();
 })();
