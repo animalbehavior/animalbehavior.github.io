3d9ea9e7c5fa1821399b7c43dcd5fe4a2b12ce92
Cole French <16979554+ColeFrench@users.noreply.github.com>
2019-06-17 07:33:38 -0400

    Add style & animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-(() => {
+(($) => {
     'use strict';
 
-    // Make body flush with top of viewport
-    (window.onresize = () => {
-        document.body.style.marginTop = 0;
-        document.body.style.marginTop = document.body.scrollHeight - document.documentElement.scrollHeight + 'px';
-    })();
-})();
+    // Bind to resize event
+    const onResize = () => {
+        // Make body flush with top of viewport
+        $(document.body).css('marginTop', 0).css('marginTop', $(document.body).outerHeight() - $(document.documentElement).outerHeight());
+
+        // Position info divs at position of main content
+        $('.info').css('left', parseFloat($(document.body).css('paddingLeft')) + parseFloat($('.content').css('marginLeft')))
+            .css('top', parseFloat($(document.body).css('paddingTop')) + parseFloat($('.content').css('marginTop')));
+    };
+    onResize();
+    $(window).resize(onResize);
+
+    // Increase grid item height on hover
+    const deltaHeight = '1vh';
+    $('.item').hover(function () {
+        $(this).animate({
+            height: '+=' + deltaHeight,
+            lineHeight: '+=' + deltaHeight
+        }, 200);
+    }, function () {
+        $(this).animate({
+            height: '-=' + deltaHeight,
+            lineHeight: '-=' + deltaHeight
+        }, 200);
+    });
+})(jQuery);
